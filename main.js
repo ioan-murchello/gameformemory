6f66f5f7b86c76fcc7c653995e5d4b1fdf4cecdf
ioan-murchello <mihalkurz@gmail.com>
2022-10-09 13:50:48 +0200

    1.version

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
-
 let hasFlipped = false;
 let boardLock = false;
+let total_curency = 0;
+let total_curency_two = 0;
 let first_card, second_card;
+let playground_wrapper = document.querySelectorAll('.playground_wrapper');
 let playground = document.querySelectorAll('.playground');
 let game = document.querySelector('.main_menu');
 let game_wrapper = document.querySelector('.game_wrapper');
+let small_menu = document.querySelector('.small_menu');
+let restart = small_menu.querySelector('.restart');
+let go_to_menu = small_menu.querySelector('.go_to_menu');
 
 game_wrapper.append(game);
 
@@ -14,12 +19,12 @@ let start_btn = document.querySelector('.btn');
 
 let arr = ['airplane','airplane','baseball','baseball','education','education','fork','fork'];
 
-let havyStage = {
-    airplane : './icons/airplane.svg',
-    baseball : './icons/baseball.svg',
-    education : './icons/education.svg',
-    fork : './icons/fork.svg'
-}
+// let havyStage = {
+//     airplane : './icons/airplane.svg',
+//     baseball : './icons/baseball.svg',
+//     education : './icons/education.svg',
+//     fork : './icons/fork.svg'
+// }
 
 let fattr = 0, secattr = 0;
 
@@ -35,110 +40,48 @@ let fattr = 0, secattr = 0;
     }
  })
 
- start_btn.addEventListener('click', function(event){
-
-    if(fattr == 1 && secattr== 12){
+ start_btn.addEventListener('click', function(){
+    startuem();
+    if(fattr == 1 && secattr== 8){
         game.classList.add('hide');
-        game_wrapper.remove(game);
-        // playground[0].classList.remove('hide');
+        game_wrapper.removeChild(game);
         setTimeout(function(){
-            game_wrapper.append(playground[0]);
-        playground[0].style.display = 'flex';
-        playground[0].classList.remove('hide');
-        },1000)
+            game_wrapper.append(playground_wrapper[0]);
+            playground_wrapper[0].classList.remove('hide');
+            playground_wrapper[0].classList.add('show');
+            }, 800)
     }
 
     if(fattr == 1 && secattr == 16){
         game.classList.add('hide');
-        game_wrapper.remove(game);
-        playground[1].querySelectorAll('.card_body').forEach(el => {
+        game_wrapper.removeChild(game);
+
+        playground_wrapper[1].querySelectorAll('.card_body').forEach(el => {
             el.classList.add('dwa');
-            setTimeout(function(){
-                game_wrapper.append(playground[1]);
-                playground[1].classList.remove('hide');
-                playground[1].style.display = 'flex';
-            }, 1000)
         });
-        
-        
-        
+
+        setTimeout(function(){
+            game_wrapper.append(playground_wrapper[1]);
+            playground_wrapper[1].classList.remove('hide');
+            playground_wrapper[1].classList.add('show');
+            }, 800)    
     }
 })
 
-function makebody(){
-        let card = document.createElement('div');
-            card.classList.add('card_body');
-      
+// --------------------------------------------------------------------
 
-        let bc = document.createElement('img');
-            bc.classList.add('back_card');
-            
+    function flipCard(e){
 
-        let fc = document.createElement('img');
-            fc.classList.add('front_card');
-           
-
-        card.append(bc);
-        card.append(fc);
-
-    return card;
-       
-}  
-
-function makeCards(num){
-    let card_body;
-    for(let i = 0; i < num; i++){
-        card_body = makebody();
-        playground.append(card_body);
-    }
-    makeAttr(havyStage, playground);
-    makeImgSrc(playground)
-}
-
-function makeAttr(arrat, play){
-   let k;
-    let cards = play.querySelectorAll('.card_body');
-            for(let j = 0; j < cards.length; j++){
-                for(let key in arrat){
-                    console.log(k)
-                    k = key
-                }
-                cards[j].setAttribute('data-card', k) 
-            }
-
-   
-        
-}
-
-function makeImgSrc(block){
-    let images = [];
-    let img = block.querySelectorAll('.back_card');
-    let n = [...img];
-    
-        for(let i = 0; i < n.length; i++){
-            
-          for(let key in havyStage){
-            images.push(havyStage[key])
-            
-          }
-          n[i].src = images[i]
-        }
-}
-
-// makeCards(8);
-
-let flipCard = (e) => {
     if(boardLock) return;
 
     let target = e.target.parentElement;
-    console.log(target)
         target.classList.add('flip')
  
     // false double click at one card
     if(target === first_card) return
 
     if(!hasFlipped){
-        //firs click
+        //first click
         hasFlipped = true;
         first_card = target;
     }
@@ -151,11 +94,32 @@ let flipCard = (e) => {
         checkForMatch()
     }
 }
-
+let isEqual;
 function checkForMatch(){
-    let isEqual = first_card.getAttribute('data-card') === second_card.getAttribute('data-card');
+    isEqual = first_card.getAttribute('data-card') === second_card.getAttribute('data-card');
+
+    if(isEqual){
+        total_curency++;
+        total_curency_two++;
+    }
     
     isEqual ? disableCards() : flippedCards();
+
+    if(fattr == 1 && secattr == 8 && total_curency == 4){
+        setTimeout(() => {
+            game_wrapper.append(small_menu);
+            small_menu.classList.add('show');
+        }, 1000)
+        
+    }
+
+    if(fattr == 1 && secattr == 16 && total_curency_two == 8){
+        setTimeout(() => {
+            game_wrapper.append(small_menu);
+            small_menu.classList.add('show');
+        }, 1000)
+        
+    }
         
 }
 
@@ -184,10 +148,36 @@ function resetBoard(){
     first_card = second_card = null;
 }
 
-let cards = document.querySelectorAll('.card_body');
+function startuem(){
+    let cards = document.querySelectorAll('.card_body');
     cards.forEach(card => {
         card.addEventListener('click', flipCard)
     
     let randomIndex = Math.floor(Math.random() * cards.length);
     card.style.order = randomIndex;
     })
+}
+
+function removeFlipClass(){
+    let all_cards = document.querySelectorAll('.card_body');
+        all_cards.forEach(el => {
+            el.classList.remove('flip');
+            el.removeEventListener('click', flipCard);
+        })
+}
+
+restart.addEventListener('click', () => {
+
+    small_menu.classList.add('hide');
+    
+    removeFlipClass();
+    
+     
+    hasFlipped = boardLock = false;
+    first_card = second_card = null;
+    total_curency = 0;
+    
+     
+    startuem();
+
+})
